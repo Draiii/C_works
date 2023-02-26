cbda86062006fc2607c144900b8706571156465e
tws <thdxodns@naver.com>
2023-02-27 03:54:50 +0900

    workss

---
+++ 06/test06.c
@@ -0,0 +1,41 @@
+
+
+#include <stdio.h>
+#define SIZE 5
+
+int main(int argc, char *argv[])
+{
+
+    int repeat, temp, i, a[SIZE] = {1, 5, 2, 3, 4};
+
+    printf("정렬전 배열 \n");
+    for (i = 0; i < SIZE; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    for (repeat = 1; repeat < SIZE; repeat++)
+    {
+        for (i = 0; i < SIZE - 1; i++)
+        {
+            if (a[i] < a[i + 1])
+            {
+                temp = a[i];
+                a[i] = a[i + 1];
+                a[i + 1] = temp;
+            }
+        }
+        for (i = 0; i < SIZE; i++)
+        {
+            printf("%d", a[i]);
+        }
+        printf("\n");
+    }
+
+    printf("\n\n정렬후 배열 \n");
+    for (i = 0; i < SIZE; i++)
+    {
+        printf("%d ", a[i]);
+    }
+    return 0;
+}
